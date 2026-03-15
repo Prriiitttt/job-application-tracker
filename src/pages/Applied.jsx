6fc86eee325b01c4cd05c5e00eb03bf53b4cbb47
Prriiitttt <pritjpatel3501@gmail.com>
2026-03-15 09:45:45 -0400

    feat: add layout for Applied page

--- src/pages/Applied.jsx
+++ src/pages/Applied.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import "./Applied.css"
 
 export default function Applied () {
   return (
-    <div>
-      sum applied
+    <div className='application'>
+      <div className="application-header">
+        <h1>My Applications</h1>
+        <button className='add-btn'>+ Add New Application</button>
+      </div>
+      <div className="applications-table"></div>
     </div>
   )
 }
